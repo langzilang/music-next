bcc38b729f6416e39a0b072fea6429c39e6e7b11
langzilang <1874815111@qq.com>
2021-05-14 17:34:33 +0800

    commit

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,17 @@
 module.exports = {
-  lintOnSave: true
+  lintOnSave: true,
+  css:{
+    loaderOptions:{
+      sass: {
+        //全局引入变量和mixin
+        additionalData:`
+        @import "@/assets/scss/variable.scss";
+        @import "@/assets/scss/base.scss";
+        @import "@/assets/scss/mixin.scss";
+        @import "@/assets/scss/reset.scss";
+        @import "@/assets/scss/icon.scss";
+        `
+      }
+    }
+  }
 }
