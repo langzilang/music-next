f62c9301e2655675e7877f86796ad5d64f686ee5
langzilang <1874815111@qq.com>
2021-10-20 08:30:32 +0800

    update

--- src/main.js
+++ src/main.js
@@ -2,13 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
-//引入全局样式文件
-import '@/assets/scss/base.scss'
-import '@/assets/scss/icon.scss'
-import '@/assets/scss/reset.scss'
+// 引入全局样式文件
+import '@/assets/scss/index.scss'
+// import '@/assets/scss/icon.scss'
+// import '@/assets/scss/reset.scss'
 // import '@/assets/scss/mixin.scss'
 // import '@/assets/scss/variable.scss'
-
 
 createApp(App).use(store).use(router).mount('#app')
--- vue.config.js
+++ vue.config.js
@@ -8,7 +8,7 @@ module.exports = {
       sass: {
         //全局引入变量和mixin
         additionalData:`
-        @import "@/assets/scss/variable.scss"
+        @import "@/assets/scss/variable.scss";
         @import "@/assets/scss/mixin.scss";
         `
       },
